f803719f49d2de917783bd9c586546907bef0fa5
Rosie Wang <34354350+roseyume@users.noreply.github.com>
2021-03-19 22:39:35 -0500

    created a features component

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Features from '../components/Features'
 
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
-            <h1>Landing Page Component</h1>
+            <h1>PetFinder</h1>
+            <Features />
         </div>
     )
 }
