02f1c88971b345a2a7dfb63272bfc04490bcfbd1
Abby Thomas <69169237+thomasabigail@users.noreply.github.com>
2021-03-23 15:18:48 -0500

    Added explanation component

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,1 +1,15 @@
-// This is where the explanation component will be created
+import '../Explanation.css';
+import React from 'react';
+
+function Explanation()
+{
+    return(
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to PetFinder!</h1>
+            <p class="lead">Our goal is to unite pets with their forever families.</p>
+            <hr class="my-4"></hr>
+        </div>
+    );
+}
+
+export default Explanation;
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Explanation from '../components/Explanation'
 
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
+            {}
             <h1>Landing Page Component</h1>
+
+            <Explanation />
         </div>
     )
 }
