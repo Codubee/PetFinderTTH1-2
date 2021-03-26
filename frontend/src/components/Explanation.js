ad6600ab89471d4978390b5afc19b0a2db361983
Abby Thomas <69169237+thomasabigail@users.noreply.github.com>
2021-03-26 17:59:14 -0500

    TTH-2-explanation: Added explanation component (#4)
    * Added explanation component
    
    * remove default landing component text
    
    * Fixed component color and moved css to styles folder
    
    * Removed unnecessary curly braces
    
    * Fixed class names
    
    * Renamed header class again
    
    Co-authored-by: codubee <info@codubee.com>

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,1 +1,15 @@
-// This is where the explanation component will be created
+import '../styles/Explanation.css';
+import React from 'react';
+
+function Explanation()
+{
+    return(
+        <div class="jumbotron">
+            <h1 class="explanationH1">Welcome to PetFinder!</h1>
+            <p class="introText">Our goal is to unite pets with their forever families.</p>
+            <hr class="headerline"></hr>
+        </div>
+    );
+}
+
+export default Explanation;
