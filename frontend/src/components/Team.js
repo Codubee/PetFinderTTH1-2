44b48ccfa9537c1634305816d582b7886e04ea49
Palak Bhargava <73029419+palak-bhargava@users.noreply.github.com>
2021-03-26 18:02:34 -0500

    TTH-4: created team component (#5)
    * Added team component files
    
    * Delete frontend/src/images directory
    
    * Rewrote Team.js by using the TeamData.json file, added new images folder to public
    
    * Changed image sizes and adjusted padding on bottom
    
    * Centered bottom two cards, moved card styling from Team.js to Team.css
    
    * Fixed the year for each member in the team component
    
    Co-authored-by: codubee <info@codubee.com>

---
+++ frontend/src/components/Team.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Card, CardImg, CardTitle, CardText,
+  CardSubtitle, CardBody, Row, Col
+} from 'reactstrap';
+import '../styles/Team.css';
+import data from '../jsonData/TeamData.json'
+
+function Team() {
+  return (
+    <div id="team">
+      <Row className="justify-content-sm-center">
+        {data.map((person, index) => (
+          <Col sm="4">
+            <div key={index}>
+              <Card body inverse id="cardStyle">
+                <CardImg top width="100%" src={person.image} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h2">{person.name}</CardTitle>
+                  <CardSubtitle tag="h4">Major:</CardSubtitle>
+                  <CardText tag="h5">{person.major}</CardText>
+                  <CardSubtitle tag="h4">School:</CardSubtitle>
+                  <CardText tag="h5">{person.school}</CardText>
+                  <CardSubtitle tag="h4">Year:</CardSubtitle>
+                  <CardText tag="h5">{person.year}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+          </Col>
+        ))}
+
+      </Row>
+    </div>
+
+  );
+};
+
+export default Team;
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Explanation from '../components/Explanation'
 import Features from '../components/Features'
+import Team from '../components/Team';
 
 function LandingPage(){
     return(
         <div>
             <Explanation />
             <Features />
+            <Team/>
         </div>
     )
 }
