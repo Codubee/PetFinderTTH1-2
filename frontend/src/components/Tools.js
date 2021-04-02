6649afa326eb5f1423abf19a0248004d39cbfb1e
ReneeAl <60901208+ReneeAl@users.noreply.github.com>
2021-04-02 15:52:07 -0500

    TTH-5: Created Tools Component and Adding it to LandingPage (#9)
    * created the tools.js, tools.css and added tools to the LandingPage
    
    * Added center div to app.css,added center div to tools, remvoved height from explanation, removed height from team, changed tools max width to min width
    
    Co-authored-by: codubee <info@codubee.com>

---
+++ frontend/src/components/Tools.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/Tools.css'
+
+function Tools() {
+    return (
+        <div className="center-div">
+            <div id="Tools">
+                <div id="ToolsText">
+                    Tools used:
+                    <ul>
+                        <li>React</li>
+                        <li>NodeJs</li>
+                        <li>Jira</li>
+                        <li>Postman</li>
+                        <li>AWS</li>
+                        <li>Heroku</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tools;
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Explanation from '../components/Explanation'
 import Features from '../components/Features'
 import Team from '../components/Team';
+import Tools from '../components/Tools'
 
 function LandingPage(){
     return(
         <div>
             <Explanation />
             <Features />
-            <Team />
+            <Team/>
+            <Tools/>
         </div>
     )
 }
