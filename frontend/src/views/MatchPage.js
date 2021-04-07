81a2f138d7f3074394603bda8cd0bcb5f74cb1e6
Palak Bhargava <palak_bhargava@hotmail.com>
2021-04-07 15:35:31 -0500

    Added yes or no buttons to MatchPage.js

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../styles/MPbuttons.css';
 
 function MatchPage(){
     return(
         <div>
             <h1>MatchPage</h1>
+            <div class="buttonGroup">
+                <button class="mp-button" id="green">YES</button>
+                <button class="mp-button" id="red">NO</button>
+            </div>
         </div>
     )
 }
