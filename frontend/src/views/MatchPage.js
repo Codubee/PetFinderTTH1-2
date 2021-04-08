d6ef9d503088eee25114fced490859303f171f12
Rosie Wang <34354350+roseyume@users.noreply.github.com>
2021-04-08 18:49:50 -0500

    Changed from carousel to single image display

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import AnimalImage from '../components/AnimalImage'
 
-var animalImageArray = [
-    {
-        src: '/images/dog1.jpg',
-        altText: 'Chow Chow lying down'
-    },
-    {
-        src: '/images/dog2.jpg',
-        altText: 'Chow Chow looking over a wall'
-    },
-    {
-        src: '/images/dog3.jpg',
-        altText: 'Chow Chow in the park'
-    }
-];
-
 function MatchPage(){
     return(
-        <div>
+        <div id="matchPage">
             <h1>MatchPage</h1>
-            <div>
-                <AnimalImage images={animalImageArray}/>
-            </div>
+            <AnimalImage image={'/images/dog1.jpg'} altText={"chow chow"}/>
         </div>
     )
 }
