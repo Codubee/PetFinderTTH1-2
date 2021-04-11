9a0e56bd417ede2e52e29be40421b15d95e6e1cb
Rosie Wang <34354350+roseyume@users.noreply.github.com>
2021-04-11 17:43:45 -0500

    TTH-10: Display animal image for Match page (#10)
    * Display animal image for Match page
    
    * Rearranged animal image component in Match page
    
    * Changed from carousel to single image display

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AnimalImage from '../components/AnimalImage'
 
 function MatchPage(){
     return(
-        <div>
+        <div id="matchPage">
             <h1>MatchPage</h1>
+            <AnimalImage image={'/images/dog1.jpg'} altText={"chow chow"}/>
         </div>
     )
 }
