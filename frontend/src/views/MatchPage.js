da94f70f2ffd535061f23d4348ab81a72dc5ace5
Palak Bhargava <73029419+palak-bhargava@users.noreply.github.com>
2021-04-12 12:52:33 -0500

    TTH-12: added yes or no buttons to MatchPage (#11)
    * Added yes or no buttons to MatchPage.js
    
    * Added hover styling and changed font to primary font
    
    * Added newlines and spaces between css items
    
    * Added margin spacing between animal image and buttons

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/MPbuttons.css';
 import AnimalImage from '../components/AnimalImage'
 
 function MatchPage(){
@@ -6,6 +7,10 @@ function MatchPage(){
         <div id="matchPage">
             <h1>MatchPage</h1>
             <AnimalImage image={'/images/dog1.jpg'} altText={"chow chow"}/>
+            <div class="buttonGroup">
+                <button class="mp-button" id="green">YES</button>
+                <button class="mp-button" id="red">NO</button>
+            </div>
         </div>
     )
 }
