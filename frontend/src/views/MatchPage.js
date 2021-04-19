45c80fe3a68414e803d79a1d5b60e51c751ecb0e
areebakq <areebakq@gmail.com>
2021-04-19 12:23:53 -0500

    added button that displays matches when pressed

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/MPbuttons.css';
 import AnimalImage from '../components/AnimalImage'
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
 
 function MatchPage(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return(
         <div id="matchPage">
             <h1>MatchPage</h1>
@@ -10,6 +16,16 @@ function MatchPage(){
             <div class="buttonGroup">
                 <button class="mp-button" id="green">YES</button>
                 <button class="mp-button" id="red">NO</button>
+            </div>
+            <div class="toggleButton">
+            <button color="primary" onClick={toggle} class="mp-button" id="blue">Show Matches</button>
+            <Collapse isOpen={isOpen}>
+                <Card>
+                <CardBody>
+                Match descriptions
+                </CardBody>
+                </Card>
+            </Collapse>
             </div>
         </div>
     )
