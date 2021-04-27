cd837e7af5661dbb7e988c66001bb01f9ed84dc1
Rosie Wang <34354350+roseyume@users.noreply.github.com>
2021-04-27 10:05:34 -0500

    added backend route to add matched animal for user

--- backend/index.js
+++ backend/index.js
@@ -3,28 +3,29 @@ const app = express()
 const axios = require('axios');
 app.use(express.json());
 
+app.get('/addAnimal', function(req,res){
 
-/*
-    I will leave this here as an example that you can use
-    when building your APIs. I suggest that you try to understand
-    what each line is doing. Reach out if you have any questions.
-*/
-app.get('/exampleApi', function (req, res) {
+    const body = {
+        "id":1,
+        "record": {
+            "name": "Brownie",
+            "type": "Labrador Retriever",
+            "sex": "Male",
+            "shelter_name": "Austin Pets Alive",
+            "adoption_url": "https://www.austinpetsalive.org/adopt/dogs/apa-a-56615",
+            "image": "https://www.austinpetsalive.org/assets/animals/_largeThumb/20190606003943.jpeg"
+        }
+    }
 
-    axios.get('https://api.agify.io?name=Mike')
+    axios.post('https://codubee-projects-api.herokuapp.com/animal/addAnimal',body)
     .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
+        console.log(response.data)
         res.status(200).json(response.data);
     })
     .catch(function (error) {
-        /* 
-            It is good practice to handle the error and send back the 
-            error message that was received from the API call
-        */ 
         console.log(error)
         res.status(400).json({error:"An error occurred"});
-    })
+    })       
 })
 
-app.listen(8080, () => console.log('Listening at locahost:8080'))
+app.listen(8080, () => console.log('Listening at localhost:8080'))
