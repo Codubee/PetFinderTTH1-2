5f2b0604614f43dcd0bdc1f6a3e284917252c426
Rosie Wang <34354350+roseyume@users.noreply.github.com>
2021-04-29 12:38:54 -0500

    Added unsaved change to take request to create response body

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,7 @@ app.use(express.json());
 
 app.post('/addAnimal', function(req,res){
 
-    const body = {
-        "id":1,
-        "record": {
-            "name": "Brownie",
-            "type": "Labrador Retriever",
-            "sex": "Male",
-            "shelter_name": "Austin Pets Alive",
-            "adoption_url": "https://www.austinpetsalive.org/adopt/dogs/apa-a-56615",
-            "image": "https://www.austinpetsalive.org/assets/animals/_largeThumb/20190606003943.jpeg"
-        }
-    }
-
-    axios.post('https://codubee-projects-api.herokuapp.com/animal/addAnimal',body)
+    axios.post('https://codubee-projects-api.herokuapp.com/animal/addAnimal',req)
     .then(function (response) {
         console.log(response.data)
         res.status(200).json(response.data);
