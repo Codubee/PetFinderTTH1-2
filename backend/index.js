af4a32a84fec665c883801086beea4ab4b8d108c
Palak Bhargava <73029419+palak-bhargava@users.noreply.github.com>
2021-04-28 21:44:14 -0500

    Added routing to get animal details from the API (#15)

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express();
 const axios = require('axios');
 app.use(express.json());
 
+app.get('/getAnimalDescription', function (req, res) {
 
-/*
-    I will leave this here as an example that you can use
-    when building your APIs. I suggest that you try to understand
-    what each line is doing. Reach out if you have any questions.
-*/
-app.get('/exampleApi', function (req, res) {
-
-    axios.get('https://api.agify.io?name=Mike')
+    axios.get('https://codubee-projects-api.herokuapp.com/animal/getAnimalDescription')
     .then(function (response) {
         // handle success and send back a 200 response with the data
         console.log(response.data);
         res.status(200).json(response.data);
     })
-    .catch(function (error) {
-        /* 
-            It is good practice to handle the error and send back the 
-            error message that was received from the API call
-        */ 
-        console.log(error)
+    .catch(function (error) { 
+        //handle error and send back error message
+        console.log(error);
         res.status(400).json({error:"An error occurred"});
     })
 })
 
-app.listen(8080, () => console.log('Listening at locahost:8080'))
+app.listen(8080, () => console.log('Listening at localhost:8080'));
