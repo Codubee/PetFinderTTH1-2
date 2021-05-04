e90908d8a272fa392e7acefd52211e8026387876
Abby Thomas <69169237+thomasabigail@users.noreply.github.com>
2021-05-04 14:01:46 -0500

    Update AnimalDescription.js

--- frontend/src/components/AnimalDescription.js
+++ frontend/src/components/AnimalDescription.js
@@ -1,9 +1,8 @@
 import '../styles/AnimalDescription.css';
 import React from 'react';
 import {
-    Card, CardImg, CardTitle, CardText,
-    CardSubtitle, CardBody, Row, Col
-  } from 'reactstrap';
+    Card, CardTitle, CardText,
+    CardSubtitle, CardBody} from 'reactstrap';
 
 const Description = {
     "name": "Ruff",
