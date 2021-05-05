62ceb9184816cbdc524a8797dd4c332317c1fbf9
codubee <info@codubee.com>
2021-05-04 23:38:50 -0500

    Added proxy and change the match to class component

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -5,28 +5,39 @@ import { Collapse, Container } from 'reactstrap';
 import ShowMatches from '../components/ShowMatches'
 import '../styles/MatchPage.css'
 
-function MatchPage() {
-    const [isOpen, setIsOpen] = useState(false);
+class MatchPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { isOpen: false }
+        this.setIsOpen = this.setIsOpen.bind(this)
+    }
 
-    const toggle = () => setIsOpen(!isOpen);
+    setIsOpen() {
+        var open = !this.state.isOpen;
+        this.setState({ isOpen: open })
+    }
 
-    return (
-        <div id="matchPage">
-            <AnimalImage image={'/images/dog1.jpg'} altText="chow chow" />
-            <div className="buttonGroup">
-                <button className="mp-button" id="green">YES</button>
-                <button className="mp-button" id="red">NO</button>
+    render() {
+
+
+        return (
+            <div id="matchPage">
+                <AnimalImage image={'/images/dog1.jpg'} altText="chow chow" />
+                <div className="buttonGroup">
+                    <button className="mp-button" id="green">YES</button>
+                    <button className="mp-button" id="red">NO</button>
+                </div>
+                <div className="toggleButton">
+                    <button onClick={this.setIsOpen} className="mp-button" id="blue">Show Matches</button>
+                    <Collapse isOpen={this.state.isOpen}>
+                        <Container>
+                            <ShowMatches />
+                        </Container>
+                    </Collapse>
+                </div>
             </div>
-            <div className="toggleButton">
-                <button onClick={toggle} className="mp-button" id="blue">Show Matches</button>
-                <Collapse isOpen={isOpen}>
-                    <Container>
-                        <ShowMatches />
-                    </Container>
-                </Collapse>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default MatchPage;
