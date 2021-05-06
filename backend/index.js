59996409f62c6e72159ed0ca66a5e82c6b85f46e
areebakq <areebakq@gmail.com>
2021-05-06 05:45:40 -0500

    added route to get weather

--- backend/index.js
+++ backend/index.js
@@ -18,4 +18,19 @@ app.get('/getAnimalDescription', function (req, res) {
     })
 })
 
+app.get('/getWeather', function (req, res) {
+
+    axios.get('https://codubee-projects-api.herokuapp.com/translate/getWeather')
+    .then(function (response) {
+        // handle success and send back a 200 response with the data
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) { 
+        //handle error and send back error message
+        console.log(error);
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
 app.listen(8080, () => console.log('Listening at localhost:8080'));
