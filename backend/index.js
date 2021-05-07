2c42bba98df7b146872017b9f234d26fa9f6eace
Rosie Wang <34354350+roseyume@users.noreply.github.com>
2021-05-07 14:31:23 -0500

    TTH-18: added backend route to add matched animal for user (#14)
    * added backend route to add matched animal for user
    
    * Changed backend route to take request to create response body
    
    * Added unsaved change to take request to create response body
    
    * Fixed error sending request instead of request body
    
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
-const app = express();
+const app = express()
 const axios = require('axios');
 app.use(express.json());
+
+app.post('/addAnimal', function(req,res){
+    axios.post('https://codubee-projects-api.herokuapp.com/animal/addAnimal',req.body)
+    .then(function (response) {
+        console.log(response.data)
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })       
+})
 
 app.get('/getAnimalDescription', function (req, res) {
 
