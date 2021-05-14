76a497a34daae3912b2d0482b5b7330cfd08c509
Codubee <70549347+Codubee@users.noreply.github.com>
2021-05-13 22:14:53 -0500

    Added changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
 const axios = require('axios');
+var cors = require('cors')
 app.use(express.json());
+app.use(cors())
+
 
 app.post('/addAnimal', function(req,res){
     axios.post('https://codubee-projects-api.herokuapp.com/animal/addAnimal',req.body)
@@ -47,4 +50,4 @@ app.get('/getWeather', function (req, res) {
     })
 })
 
-app.listen(8080, () => console.log('Listening at localhost:8080'));
+app.listen(process.env.PORT || 8080, () => console.log('Listening at localhost:8080'));
